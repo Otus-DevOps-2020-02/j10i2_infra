52cae83670f57a1b671816b00265df51a10c1a2e
j10i2 <pawelkosykh@gmail.com>
2020-04-22 17:33:08 +0300

    Actions for 1 HW Ansible

---
+++ ansible/test.py
@@ -0,0 +1,20 @@
+import json
+dictData = {
+  "app": {
+    "hosts": {
+      "appserver": {
+        "ansible_host": "34.77.35.66"
+      }
+    }
+  },
+  "db": {
+    "hosts": {
+      "dbserver": {
+        "ansible_host": "34.76.95.110"
+      }
+    }
+  }
+}
+jsonData = json.dumps(dictData)
+with open("inventory.json", "w") as file:
+    file.write(jsonData)
